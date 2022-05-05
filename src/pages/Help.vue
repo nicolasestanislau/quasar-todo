ed0cad40902d5c07dd4d44b62d40b64641ac5e11
nicolasestanislau <nicolas_estanislau@hotmail.com>
2022-05-04 23:39:24 -0300

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Ajuda</h5>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut neque turpis,
+      laoreet sed convallis vel, posuere ac risus. Maecenas eu dictum lorem.
+      Phasellus eget sem vitae dolor fermentum luctus. Nullam diam sapien,
+      vestibulum vitae ante vitae, dapibus pharetra massa. Pellentesque
+      consectetur viverra tortor, vel varius enim tincidunt id. Praesent sed
+      tellus ac velit luctus convallis. Maecenas porttitor pharetra sapien, et
+      porttitor ipsum sollicitudin eget. Ut elementum aliquet interdum. Ut
+      dapibus ante tempus enim blandit ullamcorper. Suspendisse non arcu tellus.
+      Suspendisse ac lorem posuere, faucibus lacus ac, lobortis diam. Cras vitae
+      bibendum tellus. Cras fermentum nulla quam, et laoreet nulla mollis nec.
+      Curabitur in sem efficitur, malesuada orci vel, ultrices lacus.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut neque turpis,
+      laoreet sed convallis vel, posuere ac risus. Maecenas eu dictum lorem.
+      Phasellus eget sem vitae dolor fermentum luctus. Nullam diam sapien,
+      vestibulum vitae ante vitae, dapibus pharetra massa. Pellentesque
+      consectetur viverra tortor, vel varius enim tincidunt id. Praesent sed
+      tellus ac velit luctus convallis. Maecenas porttitor pharetra sapien, et
+      porttitor ipsum sollicitudin eget. Ut elementum aliquet interdum. Ut
+      dapibus ante tempus enim blandit ullamcorper. Suspendisse non arcu tellus.
+      Suspendisse ac lorem posuere, faucibus lacus ac, lobortis diam. Cras vitae
+      bibendum tellus. Cras fermentum nulla quam, et laoreet nulla mollis nec.
+      Curabitur in sem efficitur, malesuada orci vel, ultrices lacus.
+    </p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
---
+++ src/pages/Todo.vue
@@ -0,0 +1,62 @@
+<template>
+  <q-page class="bg-grey-3 column">
+    <q-list class="bg-white" separator bordered>
+      <q-item
+        v-for="(task, index) in tasks"
+        :key="task.title"
+        @click="task.done = !task.done"
+        :class="{ 'done bg-blue-1': task.done }"
+        clickable
+        v-ripple
+      >
+        <q-item-section avatar>
+          <q-checkbox
+            class="no-pointer-events"
+            v-model="task.done"
+            color="primary"
+          />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>{{ task.title }}</q-item-label>
+        </q-item-section>
+        <q-item-section v-if="task.done" side>
+          <q-btn flat dense round color="primary" icon="delete" />
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  data() {
+    return {
+      tasks: [
+        {
+          title: "learn quasar",
+          done: false,
+        },
+        {
+          title: "make a application",
+          done: false,
+        },
+        {
+          title: "test application",
+          done: false,
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #bbb;
+  }
+}
+</style>
